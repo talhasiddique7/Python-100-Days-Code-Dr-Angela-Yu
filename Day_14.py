bf737f5eaaa16e439c2ea4333a6f9eedb79cd09b
TALHA SIDDIQUE <116684368+talhasiddique7@users.noreply.github.com>
2023-04-11 21:34:43 +0500

    Add files via upload

---
+++ Day_14.py
@@ -0,0 +1,361 @@
+# # Day 14 Higher Lower Game
+
+# # Game Data:
+# import random
+# import os
+# data = [
+#     {
+#         'name': 'Instagram',
+#         'follower_count': 346,
+#         'description': 'Social media platform',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Cristiano Ronaldo',
+#         'follower_count': 215,
+#         'description': 'Footballer',
+#         'country': 'Portugal'
+#     },
+#     {
+#         'name': 'Ariana Grande',
+#         'follower_count': 183,
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Dwayne Johnson',
+#         'follower_count': 181,
+#         'description': 'Actor and professional wrestler',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Selena Gomez',
+#         'follower_count': 174,
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kylie Jenner',
+#         'follower_count': 172,
+#         'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kim Kardashian',
+#         'follower_count': 167,
+#         'description': 'Reality TV personality and businesswoman',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Lionel Messi',
+#         'follower_count': 149,
+#         'description': 'Footballer',
+#         'country': 'Argentina'
+#     },
+#     {
+#         'name': 'Beyoncé',
+#         'follower_count': 145,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Neymar',
+#         'follower_count': 138,
+#         'description': 'Footballer',
+#         'country': 'Brasil'
+#     },
+#     {
+#         'name': 'National Geographic',
+#         'follower_count': 135,
+#         'description': 'Magazine',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Justin Bieber',
+#         'follower_count': 133,
+#         'description': 'Musician',
+#         'country': 'Canada'
+#     },
+#     {
+#         'name': 'Taylor Swift',
+#         'follower_count': 131,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kendall Jenner',
+#         'follower_count': 127,
+#         'description': 'Reality TV personality and Model',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Jennifer Lopez',
+#         'follower_count': 119,
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Nicki Minaj',
+#         'follower_count': 113,
+#         'description': 'Musician',
+#         'country': 'Trinidad and Tobago'
+#     },
+#     {
+#         'name': 'Nike',
+#         'follower_count': 109,
+#         'description': 'Sportswear multinational',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Khloé Kardashian',
+#         'follower_count': 108,
+#         'description': 'Reality TV personality and businesswoman',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Miley Cyrus',
+#         'follower_count': 107,
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Katy Perry',
+#         'follower_count': 94,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kourtney Kardashian',
+#         'follower_count': 90,
+#         'description': 'Reality TV personality',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Kevin Hart',
+#         'follower_count': 89,
+#         'description': 'Comedian and actor',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Ellen DeGeneres',
+#         'follower_count': 87,
+#         'description': 'Comedian',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Real Madrid CF',
+#         'follower_count': 86,
+#         'description': 'Football club',
+#         'country': 'Spain'
+#     },
+#     {
+#         'name': 'FC Barcelona',
+#         'follower_count': 85,
+#         'description': 'Football club',
+#         'country': 'Spain'
+#     },
+#     {
+#         'name': 'Rihanna',
+#         'follower_count': 81,
+#         'description': 'Musician and businesswoman',
+#         'country': 'Barbados'
+#     },
+#     {
+#         'name': 'Demi Lovato',
+#         'follower_count': 80,
+#         'description': 'Musician and actress',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': "Victoria's Secret",
+#         'follower_count': 69,
+#         'description': 'Lingerie brand',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Zendaya',
+#         'follower_count': 68,
+#         'description': 'Actress and musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Shakira',
+#         'follower_count': 66,
+#         'description': 'Musician',
+#         'country': 'Colombia'
+#     },
+#     {
+#         'name': 'Drake',
+#         'follower_count': 65,
+#         'description': 'Musician',
+#         'country': 'Canada'
+#     },
+#     {
+#         'name': 'Chris Brown',
+#         'follower_count': 64,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'LeBron James',
+#         'follower_count': 63,
+#         'description': 'Basketball player',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Vin Diesel',
+#         'follower_count': 62,
+#         'description': 'Actor',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Cardi B',
+#         'follower_count': 63,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'David Beckham',
+#         'follower_count': 62,
+#         'description': 'Footballer',
+#         'country': 'United Kingdom'
+#     },
+#     {
+#         'name': 'Billie Eilish',
+#         'follower_count': 61,
+#         'description': 'Musician',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Justin Timberlake',
+#         'follower_count': 58,
+#         'description': 'Musician and actor',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'UEFA Champions League',
+#         'follower_count': 58,
+#         'description': 'Club football competition',
+#         'country': 'Europe'
+#     },
+#     {
+#         'name': 'NASA',
+#         'follower_count': 56,
+#         'description': 'Space agency',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Emma Watson',
+#         'follower_count': 56,
+#         'description': 'Actress',
+#         'country': 'United Kingdom'
+#     },
+#     {
+#         'name': 'Shawn Mendes',
+#         'follower_count': 56,
+#         'description': 'Musician',
+#         'country': 'Canada'
+#     },
+#     {
+#         'name': 'Virat Kohli',
+#         'follower_count': 55,
+#         'description': 'Cricketer',
+#         'country': 'India'
+#     },
+#     {
+#         'name': 'Gigi Hadid',
+#         'follower_count': 52,
+#         'description': 'Model',
+#         'country': 'United States'
+#     },
+#     {
+#         'name': 'Priyanka Chopra Jonas',
+#         'follower_count': 52,
+#         'description': 'Actress and musician',
+#         'country': 'India'
+#     },
+#     {
+#         'name': '9GAG',
+#         'follower_count': 52,
+#         'description': 'Social media platform',
+#         'country': 'China'
+#     },
+#     {
+#         'name': 'Ronaldinho',
+#         'follower_count': 51,
+#         'description': 'Footballer',
+#         'country': 'Brasil'
+#     },
+#     {
+#         'name': 'Maluma',
+#         'follower_count': 50,
+#         'description': 'Musician',
+#         'country': 'Colombia'
+#     },
+#     {
+#         'name': 'Camila Cabello',
+#         'follower_count': 49,
+#         'description': 'Musician',
+#         'country': 'Cuba'
+#     },
+#     {
+#         'name': 'NBA',
+#         'follower_count': 47,
+#         'description': 'Club Basketball Competition',
+#         'country': 'United States'
+#     }
+# ]
+
+# logo = """
+#     __  ___       __             
+#    / / / (_)___ _/ /_  ___  _____
+#   / /_/ / / __ `/ __ \/ _ \/ ___/
+#  / __  / / /_/ / / / /  __/ /    
+# /_/ ///_/\__, /_/ /_/\___/_/     
+#    / /  /____/_      _____  _____
+#   / /   / __ \ | /| / / _ \/ ___/
+#  / /___/ /_/ / |/ |/ /  __/ /    
+# /_____/\____/|__/|__/\___/_/     
+# """
+
+# vs = """
+#  _    __    
+# | |  / /____
+# | | / / ___/
+# | |/ (__  ) 
+# |___/____(_)
+# """
+
+
+# check=False
+# score=0
+# while not check:
+#     r1=random.choice(data)
+#     r2=r1
+#     r1=random.choice(data)
+#     if r1==r2:
+#         r1=random.choice(data)
+#     # print(r1)
+#     # print(r2)
+#     print(logo)
+#     print(f"Compare A: {r1['name']},a {r1['description']}, from {r1['country']}.")
+#     print(vs)
+#     print(f"Compare B: {r2['name']},a {r2['description']}, from {r2['country']}.")
+    
+#     input1=input("Who has More Followers? Type A or B: ")
+#     if input1=="A":
+#         if r1['follower_count']>r2['follower_count']:
+#             score+=1
+#             os.system("cls")
+#             print(f"Your're right! Current score: {score}.")
+#         else:
+#             print(f"sorry,that's wrong.Final score: {score}")
+#             break
+#     elif input1=="B":
+#         if r1['follower_count']<r2['follower_count']:
+#             score+=1
+#             os.system("cls")
+#             print(f"Your're right! Current score: {score}.")
+#         else:
+#             print(f"sorry,that's wrong.Final score: {score}")
+#             break
---
+++ Day_17.py
@@ -0,0 +1,13 @@
+# Day 17 OOP Concept to make Quiz app
+
+# class Name:      #create class name (Name)
+#     # def __init__(self):         #default constructor
+#     #     print("Hello")
+#     def __init__(self,user,id):   #1-argument
+#         self.User=user
+#         self.Id=id
+
+# object1=Name("User","01")      #create object of that class
+
+# print(object1.User)
+# print(object1.Id)
